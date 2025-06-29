dfe6b33a351023eaebf4831ffa6ab68d647add10
Philip Nwachukwu <docphils64@gmail.com>
2025-06-29 02:39:31 +0100

    Fixed in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
         if (tempQuery.trim() === "") {
             handleClear();
         }
-    }, [tempQuery]);
+    }, [tempQuery, handleClear]);
 
     const NavLink = ({to, children}) => (
         <Link
